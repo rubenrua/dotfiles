48722ff1b95f69cdfc9dc5a715e253fecb274770
Ruben Gonzalez <rubenrua@gmail.com>
2015-01-19 07:49:58 +0100

    Update emacs init.el adding melba repository

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,7 @@
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
+
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
