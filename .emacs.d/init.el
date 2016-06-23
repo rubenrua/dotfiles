76bad4ac71f8606d5506b0acb602e2965abd7b3d
Ruben Gonzalez <rubenrua@gmail.com>
2016-06-23 18:04:51 +0200

    Update emacs init.el file
    This file needs more love :(

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,9 @@
 (global-font-lock-mode 1)
 
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+(setq-default tab-width 4)
+
+(global-set-key (kbd "C-f") 'dabbrev-expand)
 
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
