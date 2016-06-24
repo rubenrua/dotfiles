a590df4b458a393d6d2957dece102719a39004b3
Ruben Gonzalez <rubenrua@gmail.com>
2016-06-24 11:45:54 +0200

    Highlight matching parentheses
    http://emacsredux.com/blog/2013/04/01/highlight-matching-parentheses/

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,12 @@
 
 (global-set-key (kbd "C-f") 'dabbrev-expand)
 
+;; MELPA
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+
+;; Highlight matching parentheses
+(require 'paren)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode +1)
